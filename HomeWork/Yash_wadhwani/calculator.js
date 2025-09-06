403ca7185796d59c4cadf4c4e0e14c4a639fa103
Yashu10-wq <yashwadhwani7867@gmail.com>
2025-09-06 17:27:21 +0530

    messege

---
+++ HomeWork/Yash_wadhwani/calculator.js
@@ -0,0 +1,38 @@
+function addition( a,b){
+    console.log("The addition of two numbers is " ,a+b);
+
+
+}
+function subtraction( a,b){
+    console.log("The subtraction of two numbers is " ,a-b);
+
+
+}
+function multiplication( a,b){
+    console.log("The multiplication  of two numbers is " ,a*b);
+
+
+}
+function divide( a,b){
+    console.log("The division of two numbers is " ,a/b);
+
+
+}
+function power( a,b){
+    console.log("a to the power b  is " ,a**b);
+
+
+}
+function modulus( a,b){
+    console.log("The modulu of two numbers is " ,a%b);
+
+
+}
+let a =4;
+let b =9;
+addition(a,b);
+subtraction(a,b);
+multiplication(a,b);
+divide(a,b);
+modulus(a,b);
+power(a,b);
---
+++ HomeWork/Yash_wadhwani/reduce.js
@@ -0,0 +1,3 @@
+const arr=[5,10,15,20];
+let sum=arr.reduce((total,number)=>total+number,0);
+console.log(sum);
